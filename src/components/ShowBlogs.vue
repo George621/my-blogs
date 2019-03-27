7ee527be3b98df0a370daf6035aeaafcd192ab1d
George621 <384515231@qq.com>
2019-03-27 15:04:55 +0800

    add router search

--- src/components/ShowBlogs.vue
+++ src/components/ShowBlogs.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="show-blogs">
+  <div id="show-blogs" v-theme:column="'narrow'">
     <h1>博客总览</h1>
-    <div class="single-blog" v-for="(blog, index) in blogs" :key="index">
-      <h3>{{blog.title}}</h3>
-      <article>{{blog.body}}</article>
+    <input type="text" placeholder="search" v-model="search">
+    <div class="single-blog" v-for="(blog, index) in filteredBlogs" :key="index">
+     <router-link v-bind:to="'/blog/'+blog.id" >
+       <h3 v-rainbow>{{blog.title | to-uppercase}}</h3>
+       <article>{{blog.body | snippet}}</article>
+     </router-link>
     </div>
   </div>
 </template>
@@ -13,28 +16,64 @@ export default {
   name:'show-blogs',
   data(){
     return {
-      blogs:[]
+      blogs:[],
+      search:''
     }
   },
   created() {
     this.$http.get('../../static/post.json')
     .then(function (data) {
      this.blogs = data.body.slice(0,10)
-      console.log(this.blogs)
     })
   },
+  computed: {
+    filteredBlogs:function () {
+      return this.blogs.filter((blog)=>{
+        return blog.title.match(this.search);
+      })
+    }
+  },
+  filters:{
+    // "to-uppercase":function (val) {
+    //   return val.toUpperCase();
+    // },
+    toUppercase(val) {
+      return val.toUpperCase();
+    },
+    "snippet":function (val) {
+      return val.slice(0,100) + '...'
+    }
+  },
+  directives:{
+    "rainbow":{
+      bind(el,binding,vnode){
+        el.style.color='#' + Math.random().toString().slice(2,8);
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
   #show-blogs{
     max-width: 800px;
-    margin: 1 auto;
+    margin: 0 auto;
   }
   .single-blog{
     padding: 20px;
     margin: 20px 0;
     box-sizing: border-box;
     background-color: #EEE;
+    border: 1px dotted #aaa;
+
+  }
+  #show-blogs a{
+    color: #444;
+    text-decoration: none;
+  }
+  input[type='text']{
+    padding: 8px;
+    width: 100%;
+    box-sizing: border-box;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,51 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
+// Vue.directive('rainbow',{
+//   bind(el,binding,vnode){
+//     el.style.color = '#'+Math.random().toString().slice(2,8)
+//   }
+// })
+// 自定义指令  |  全局
+Vue.directive('theme',{
+  bind(el,binding,vnode){
+    if(binding.value =='wide'){
+      el.style.maxWidth = "1260px"
+    }else if(binding.value =='narrow'){
+      el.style.maxWidth = "560px"
+    }
+    if(binding.arg == 'column'){
+      el.style.background = "#6677cc";
+      el.style.padding='20px';
+    }
+  }
+})
+// 自定义过滤器  |  全局 单个组件有 了就注释
+// Vue.filter('to-uppercase',function (val) {
+//   return val.toUpperCase();
+// })
+// Vue.filter('snippet',function (val) {
+//   return val.slice(0,100) + '...'
+// })
+
 /* eslint-disable no-new */
+
+//路由
+const router = new VueRouter({
+  routes: Routes,
+  mode:'history'
+})
+
+
 new Vue({
   el: '#app',
+  router: router,
   components: { App },
   template: '<App/>'
 })
